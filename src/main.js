504bc0b67d47e53fcefa28a7b78f4c9e90282f27
Sergio <serancaslan@gmail.com>
2025-10-25 20:00:58 +0000

    Users page - Add new user

--- src/main.js
+++ src/main.js
@@ -5,12 +5,22 @@ import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 import './styles.css';
 
+import ConfirmationService from 'primevue/confirmationservice'
+import ToastService from 'primevue/toastservice'
+
 const app = createApp(App)
 
 app.use(router)
 app.use(PrimeVue, {
     theme: {
-        preset: Aura
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
     }
 });
+app.use(ConfirmationService)
+app.use(ToastService)
 app.mount('#app')
--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -1,8 +1,17 @@
 <script setup>
-  import { DataTable, Column, InputText, Card, Button } from 'primevue';
+  import { DataTable, Column, InputText, Card, Button, useConfirm, 
+    useToast, Toast, ConfirmDialog, Dialog, Password, Message } from 'primevue';
   import { ref, onMounted } from 'vue';
+  import {zodResolver} from "@primevue/forms/resolvers/zod";
+  import { Form } from '@primevue/forms';
+  import {z} from 'zod';
+
+  const confirm = useConfirm();
+  const toast = useToast();
   const users = ref([]);
   const editingRows = ref([]);
+  const showForm = ref(false)
+  const initialValues = ref({username:'', password:''})
   onMounted(async () => {
     try{
       const res = await fetch('http://10.0.0.6:3000/users');
@@ -16,11 +25,93 @@
     let { newData, index } = event;
     users.value[index] = newData;
   };
+  const confirm1 = () => {
+    confirm.require({
+      message: 'Do you want to delete this user?',
+      header: 'Danger Zone',
+      icon: 'pi pi-info-circle',
+      rejectLabel: 'Cancel',
+      rejectProps: {
+          label: 'Cancel',
+          severity: 'secondary',
+          outlined: true
+      },
+      acceptProps: {
+            label: 'Delete',
+            severity: 'danger'
+      },
+      accept: () => {
+            toast.add({ severity: 'info', summary: 'Confirmed', detail: 'User deleted', life: 3000 });
+      },
+      reject: () => {
+          toast.add({ severity: 'error', summary: 'Rejected', detail: 'Action canceled', life: 3000 });
+      }
+    });
+  }
+
+  const resolver = ref(zodResolver(
+    z.object({
+        username: z.string().min(1, { message: 'Username is required.' }),
+        password: z
+            .string()
+            .min(3, { message: 'Minimum 3 characters.' })
+            .max(8, { message: 'Maximum 8 characters.' })
+            .refine((value) => /[a-z]/.test(value), {
+                message: 'Must have a lowercase letter.'
+            })
+            .refine((value) => /[A-Z]/.test(value), {
+                message: 'Must have an uppercase letter.'
+            })
+            .refine((value) => /\d/.test(value), {
+                message: 'Must have a number.'
+            })
+    })
+  ));
+
+  const addUser = async (e) => {
+    if (e.valid) {
+        toast.add({ severity: 'success', summary: 'Form is submitted.', life: 3000 });
+      console.log(e);
+      const {username, password} = e.values;
+      try {
+        const response = await fetch('http://10.0.0.6:3000/users',{
+          method: 'POST',
+          headers: {
+            'Content-Type':'application/json',
+          },
+          body: JSON.stringify({
+            username: username,
+            password: password,
+          }),
+        })
+        if (!response.ok){
+          throw new Error('Failed to add user')
+        }
+        const newUser = await response.json();
+        users.value.push(newUser);
+        showForm.value =false;
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
 </script>
 
 <template>
+  <Toast/>
+  <ConfirmDialog></ConfirmDialog>
   <Card>
-    <template #title>Users</template>
+    <template #title>
+      <div class="flex justify-between items-center">
+        <span>Users</span>
+        <Button
+          icon="pi pi-plus"
+          label="Add User"
+          class="p-button-sm"
+          @click="showForm = true"
+        />
+      </div>
+    </template>
     <template #content>
       <DataTable v-model:editingRows="editingRows" :value="users" editMode="row" dataKey="id" @row-edit-save="onRowEditSave" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle=""
         :pt="{
@@ -46,13 +137,38 @@
               severity="danger"
               class="p-button-text"
               rounded
-              @click="deleteUser(data.id)"
+              @click="confirm1()"
             />
           </template>
         </Column>
       </DataTable>
     </template>
   </Card>
-  
+
+  <Dialog v-model:visible="showForm" modal header="Add User" :style="{ width: '25rem' }">
+    <Form v-slot="$form" :initialValues :resolver @submit="addUser">
+      <div class="flex items-center gap-4 mb-4">
+        <label for="username" class="font-semibold w-24">Username</label>
+        <InputText name="username" class="flex-auto" autocomplete="off" />
+      </div>
+      <Message v-if="$form.username?.invalid" severity="error" size="small" variant="simple" class="mb-4 ml-30">{{ $form.username.error.message }}</Message>
+      <div class="flex items-center gap-4 mb-4">
+          <label for="password" class="font-semibold w-24">Password</label>
+          <div class="flex flex-col grow">
+            <Password name="password" placeholder="Password" :feedback="false" toggleMask fluid />
+          </div>
+      </div>
+      <Message v-if="$form.password?.invalid" severity="error" size="small" variant="simple" class="mb-4 ml-30">
+        <ul class="my-0 flex flex-col gap-1">
+          <li v-for="(error, index) of $form.password.errors" :key="index">{{ error.message }}</li>
+        </ul>
+      </Message>
+      
+      <div class="flex justify-end gap-2">
+          <Button type="button" label="Cancel" severity="secondary" @click="showForm = false"></Button>
+          <Button type="submit" label="Save"></Button>
+      </div>
+    </Form>
+  </Dialog>
 </template>
 <style scoped></style>
